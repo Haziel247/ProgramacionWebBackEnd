e3c69dc706617b4f229620c64cb87265a0b4a014
Haziel247 <kauhaziel@gmail.com>
2025-03-01 01:56:16 -0500

    Resubida de Ejercicio peticionHTTP, errores de ramas

--- Unidad 1/Ejercicios/1.3 Ejercicios/peticionHTTP/index.js
+++ Unidad 1/Ejercicios/1.3 Ejercicios/peticionHTTP/index.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
     res.send("<p> respuesta GET </p>");
 });
 
+app.post('/registro', (req, res)=>{
+    res.status(201).send('Registro exitoso');
+});
+
+app.put('/usuario/actualizar', (req, res)=>{
+    res.status(200).send('Actualización exitosa');
+});
+
+app.patch('/usuario/modificar', (req, res)=>{
+    res.status(200).send('Modificación exitosa');
+});
+
+app.delete('/usuario/eliminar', (req, res)=>{
+    res.status(200).send('Eliminación exitosa');
+});
+
 app.listen(port, () => {
     console.log('Server is running in port 3000');
 });
